4457a8d3abb7dbf43cca61d8d2bf53543950e1f5
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-08 14:13:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def game_board(board)
+  puts " #{board.board[0]} | #{board.board[1]} | #{board.board[2]}"
+  puts "-------------"
+  puts " #{board.board[3]} | #{board.board[4]} | #{board.board[5]}"
+  puts "-------------"
+  puts " #{board.board[6]} | #{board.board[7]} | #{board.board[8]}"
+  puts "-------------"
+end
